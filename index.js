aefe590a1dd74d4841ad14a9c32fa33fd4f68405
Pauline-momanyi <paulinemomanyi55@gmail.com>
2022-04-07 18:50:26 +0300

    Built in math objects

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1*num2;
+
+const random = Math.ceil(Math.random() * 10);
+
+const num3=9;
+const num4=5;
+const mod = num3%num4;
+
+const max = Math.floor(Math.random() +20);
+
